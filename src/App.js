150d123309daf9ba0bd01becd0b117fcc813a68a
jbovee <Mitouch.b@gmail.com>
2018-06-05 17:48:11 -0400

    Remove header. Change title size and color

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
         <h1 className="App-title">Welcome to React</h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
