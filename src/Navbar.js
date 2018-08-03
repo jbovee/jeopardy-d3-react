bfb316549f01719f36cad46e28bdbd99be668526
jbovee <Mitouch.b@gmail.com>
2018-08-03 16:19:51 -0400

    Add second navbar for mobile, and media queries to switch between the two

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
-import SeasonSlider from './SeasonSlider'
+import SeasonSlider from './SeasonSlider';
+import MediaQuery from 'react-responsive';
+import { Button, FormGroup, Input, Navbar, Nav, NavItem } from 'reactstrap';
 
-class Navbar extends Component {
+class NavBar extends Component {
+	constructor(props) {
+		super(props);
+		this.season = React.createRef();
+		this.state = {
+			all: false,
+			dropdownOpen: false,
+			season: 1
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.buttonToggle = this.buttonToggle.bind(this);
+	}
+
+	handleChange(e) {
+		this.props.handlerFromParent(false, e.target.value);
+		this.setState({
+			season: e.target.value
+		});
+	}
+
+	buttonToggle() {
+		const all = !this.state.all;
+		this.props.handlerFromParent(all, this.state.season);
+		this.setState({
+			all: all
+		});
+	}
+
 	render() {
 		return (
-            <nav id="nav" class="navbar fixed-top navbar-dark bg-dark">
-                <div className="mx-auto">
-                    <SeasonSlider startSeason={1} handlerFromParent={this.props.handlerFromParent} />
-                </div>
-            </nav>
+            <div>
+                <MediaQuery query="(min-device-width: 1224px)">
+					<Navbar color="dark" dark fixed="top">
+						<Nav className="mx-auto" navbar>
+                            <SeasonSlider startSeason={1} handlerFromParent={this.props.handlerFromParent} />
+						</Nav>
+					</Navbar>
+                </MediaQuery>
+                <MediaQuery query="(max-device-width: 1224px)">
+					<Navbar color="dark" dark fixed="top">
+						<Nav className="mx-auto" navbar>
+							<NavItem>
+								<FormGroup>
+									<Input type="select" name="season" id="season-select" disabled={this.state.all} onChange={this.handleChange} value={this.state.season}>
+										{[...Array(34).keys()].map(n => <option value={n+1}>{n+1}</option>)}
+									</Input>
+								</FormGroup>
+							</NavItem>
+							<NavItem>
+								<Button color="secondary" active={this.state.all} onClick={this.buttonToggle}>All</Button>
+							</NavItem>
+						</Nav>
+					</Navbar>
+                </MediaQuery>
+            </div>
 		);
 	}
 }
 
-export default Navbar;
+export default NavBar;
